925fdde0d82744c1e965e4b75d69a85cf14e938c
Lyr0422 <1013016663@qq.com>
2019-11-08 11:34:35 +0800

    1108-ssm+maven项目搭建完成

--- lyr-work/SSMMavenDemo01/src/main/java/com/lyr/service/Impl/AccountServiceImpl.java
+++ lyr-work/SSMMavenDemo01/src/main/java/com/lyr/service/Impl/AccountServiceImpl.java
@@ -1,7 +1,9 @@
 package com.lyr.service.Impl;
 
+import com.lyr.dao.AccountDao;
 import com.lyr.po.Account;
 import com.lyr.service.AccountService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -9,14 +11,18 @@ import java.util.List;
 @Service("accountService")
 public class AccountServiceImpl implements AccountService {
 
+    @Autowired
+    private AccountDao accountDao;
+
     @Override
     public List<Account> findAll() {
         System.out.println("Service业务层：查询所有账户...");
-        return null;
+        return accountDao.findAll();
     }
 
     @Override
     public void saveAccount(Account account) {
         System.out.println("Service业务层：保存帐户...");
+        accountDao.saveAccount(account);
     }
 }
